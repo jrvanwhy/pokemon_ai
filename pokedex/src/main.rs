6070cd292ab10f68304c917b1d596282000ec561
Johnathan Van Why <jrvanwhy@gmail.com>
2015-07-28 18:38:10 -0700

    Modify main to match the Pokedex changes (pass in the csv path).

--- pokedex/src/main.rs
+++ pokedex/src/main.rs
@@ -1,7 +1,7 @@
 mod pokedex;
 
 fn main() {
-	let pd = pokedex::Pokedex::new();
+	let pd = pokedex::Pokedex::new("/home/ryan/repos/pokemon_ai/csv/".to_string());
 	println!("{:?}", pd.get_poke_desc(10));
 	println!("{:?}", pd.get_poke_desc(15));
 }
