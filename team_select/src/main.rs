eff010bd4a12542e7523cccc22be1bb27eaef850
Johnathan Van Why <jrvanwhy@gmail.com>
2015-08-09 00:02:35 -0700

    Starting random team generation function.

--- team_select/src/main.rs
+++ team_select/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
+extern crate battle_sim;
+use battle_sim::Team;
+
+// Function to randomize the Pokemon in the given team
+fn randomize_team(team: &mut Team)
+{
+	// Re-initialize team from scratch
+	*team = Team::new();
+}
+
+fn main()
+{
 	
 }
